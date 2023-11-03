eddc76a8b1c7aaefcbf0f8afe8809a57a8745338
JunnieLee <heunghari@naver.com>
2023-11-03 17:42:43 +0900

    feat: Content viewholder 생성

---
+++ app/src/main/java/com/example/todoapp/ContentViewHolder.kt
@@ -0,0 +1,20 @@
+package com.example.todoapp
+
+import android.graphics.Paint
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todoapp.databinding.ItemContentBinding
+import com.example.todoapp.model.ContentEntity
+
+class ContentViewHolder(
+    private val binding:ItemContentBinding
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(item: ContentEntity){
+        binding.item = item
+        binding.contentCheckBox.paintFlags = if (item.isDone){
+            binding.contentCheckBox.paintFlags + Paint.STRIKE_THRU_TEXT_FLAG
+        } else {
+            0
+        }
+    }
+}
