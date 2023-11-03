7f31f10f2ec2be90ce36f0f20c65b8bd69acc503
JunnieLee <heunghari@naver.com>
2023-11-03 16:16:24 +0900

    feat: InputActivity 생성

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.todoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply{
+            setContentView(root)
+            view = this@MainActivity
+        }
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClickAdd(){
+
     }
 }
