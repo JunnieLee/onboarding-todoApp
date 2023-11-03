3f2c2e537d109d4c6df4e3aeeabe517f251a8984
JunnieLee <heunghari@naver.com>
2023-11-03 17:28:07 +0900

    feat: InputActivity에 viewModel 추가

--- app/src/main/java/com/example/todoapp/InputActivity.kt
+++ app/src/main/java/com/example/todoapp/InputActivity.kt
@@ -2,19 +2,32 @@ package com.example.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import com.example.todoapp.databinding.ActivityInputBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class InputActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityInputBinding
+
+    private val viewModel : InputViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityInputBinding.inflate(layoutInflater).apply{
             setContentView(root)
+            lifecycleOwner = this@InputActivity // viewModel에서 lifecycle 사용하니까 lifecycle owener 등록해줌
+            viewModel = this@InputActivity.viewModel
         }
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        viewModel.doneEvent.observe(this){
+            Toast.makeText(this, "완료", Toast.LENGTH_SHORT).show()
+            finish()
+        }
     }
 
     override fun onSupportNavigateUp():Boolean{
