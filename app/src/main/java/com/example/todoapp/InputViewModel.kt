5aab37650eacd1cabbbcb694707b46db68a62580
JunnieLee <heunghari@naver.com>
2023-11-03 17:22:22 +0900

    feat: Input쪽 뷰모델 생성 및 레이아웃 코드와 뷰모델 연동

---
+++ app/src/main/java/com/example/todoapp/InputViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.todoapp
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.todoapp.model.ContentEntity
+import com.example.todoapp.repository.ContentRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class InputViewModel @Inject constructor(
+    private val contentRepository: ContentRepository
+): ViewModel(){
+    private val _doneEvent = MutableLiveData<Unit>()
+    val doneEvent : LiveData<Unit> = _doneEvent
+
+    var content = MutableLiveData<String>()
+    var memo = MutableLiveData<String?>()
+
+    fun insertData(){
+        content.value?.let{content->
+                viewModelScope.launch(Dispatchers.IO){
+                    contentRepository.insert(
+                        ContentEntity(content=content, memo=memo.value)
+                    ) // suspend function이기 때문에 요 insert가 다 실행되고 난 뒤에 아래 postValue가 호출된다.
+                    _doneEvent.postValue(Unit)
+                }
+        }
+    }
+}
