bc670b4ebcd0771d9abaea85e7ca2ec82cacd744
JunnieLee <heunghari@naver.com>
2023-11-03 17:57:18 +0900

    feat: 데이터를 가져올 수 있도록 loadList 구현

--- app/src/main/java/com/example/todoapp/data/dao/ContentDao.kt
+++ app/src/main/java/com/example/todoapp/data/dao/ContentDao.kt
@@ -3,10 +3,15 @@ package com.example.todoapp.data.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.example.todoapp.model.ContentEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ContentDao {
+
+    @Query("SELECT * FROM Content")
+    fun selectAll(): Flow<List<ContentEntity>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(item:ContentEntity)
--- app/src/main/java/com/example/todoapp/repository/ContentRepository.kt
+++ app/src/main/java/com/example/todoapp/repository/ContentRepository.kt
@@ -1,9 +1,11 @@
 package com.example.todoapp.repository
 
 import com.example.todoapp.model.ContentEntity
+import kotlinx.coroutines.flow.Flow
 
 interface ContentRepository {
 
+    fun loadList(): Flow<List<ContentEntity>>
     suspend fun insert(item:ContentEntity)
 
 }
